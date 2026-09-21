1340d88c641b25e3e6e729401db0c29167970721
GARIKIPATI TEJASWI <2400032793@kluniversity.in>
2026-09-21 22:18:49 +0530

    Add files via upload

---
+++ eurekaserverapp/src/main/java/com/klu/EurekaserverappApplication.java
@@ -0,0 +1,14 @@
+package com.klu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaserverappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaserverappApplication.class, args);
+	}
+
+}
